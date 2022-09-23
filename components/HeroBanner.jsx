c074666af7ef554e34bfb291c8787cf4e9098628
justinn79 <justinlylemiraflor@gmail.com>
2022-09-23 17:46:07 -0400

    changed banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -6,9 +6,13 @@ import { urlFor } from '../lib/client';
 const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+        <div id="stars4"></div>
         <div>
-            <p className="hero-banner-product">{heroBanner.smallText}</p>
             <h3>{heroBanner.midText}</h3>
+            <p className="hero-banner-product">{heroBanner.smallText}</p>
             <h1>{heroBanner.largeText1}</h1>
         </div>
         <img src={urlFor(heroBanner.image)} alt="image" className="hero-banner-image"/>
